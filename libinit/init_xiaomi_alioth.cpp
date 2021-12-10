ea5967a4d24695eca6525b1158a42e045bd6c63b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-10 01:01:53 +0100

    alioth: Update blobs from V12.5.4.0.RKHMIXM
    Change-Id: I52f47ea15140af63ba9f0b0a7b9d237a25abdbdb

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -11,19 +11,6 @@
 
 #define FINGERPRINT "google/redfin/redfin:12/SP1A.211105.003/7757856:user/release-keys"
 
-static const variant_info_t aliothcn_info = {
-    .hwc_value = "CN",
-    .sku_value = "",
-
-    .brand = "Redmi",
-    .device = "alioth",
-    .marketname = "K40",
-    .model = "M2012K11AC",
-    .build_fingerprint = FINGERPRINT,
-
-    .nfc = true,
-};
-
 static const variant_info_t aliothin_info = {
     .hwc_value = "INDIA",
     .sku_value = "",
@@ -37,7 +24,7 @@ static const variant_info_t aliothin_info = {
     .nfc = false,
 };
 
-static const variant_info_t alioth_info = {
+static const variant_info_t alioth_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
 
@@ -50,9 +37,22 @@ static const variant_info_t alioth_info = {
     .nfc = true,
 };
 
+static const variant_info_t alioth_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "alioth",
+    .marketname = "K40",
+    .model = "M2012K11AC",
+    .build_fingerprint = FINGERPRINT,
+
+    .nfc = true,
+};
+
 static const std::vector<variant_info_t> variants = {
-    aliothcn_info,
     aliothin_info,
+    alioth_global_info,
     alioth_info,
 };
 
